28ce87ce7d6b9efc7eeb5d0d013d052ab55ae8d6
TranTruong <truongtv1909@gmail.com>
2019-08-18 21:31:24 +0700

    add method say for Cat class

--- Cat.js
+++ Cat.js
@@ -4,5 +4,8 @@ this.stomach = [];
 }
 Cat.prototype.eat = function(mouse){
 this.stomach.push(mouse);
-}
+};
+Cat.prototype.say = function(){
+console.log('My name is: '+this.name);
+};
 module.exports = Cat;
