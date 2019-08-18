1f18cabd8c6cb5cc808217ca2e36ef06649a1fa4
TranTruong <truongtv1909@gmail.com>
2019-08-18 20:46:07 +0700

    add method: eat, bark. Fix: export.

--- Dog.js
+++ Dog.js
@@ -1,3 +1,12 @@
-function Dog(){
-    
+function Dog(n,w){
+    this.name = n;
+    this.weight =w;
+    this.stomach = [];
 }
+Dog.prototype.eat =function(){
+    this.stomach.push(cat);
+}
+Dog.prototype.bark = function(){
+    console.log("Gru gru.....")
+}
+module.exports = Dog;
